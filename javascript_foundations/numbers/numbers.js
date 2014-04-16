073e41cdba2e096ce37d4c40e09eafaea933a2be
Kaustubh <kaustubh.cool@gmail.com>
2014-04-16 19:05:09 +0530

    Numbers in javascript

---
+++ javascript_foundations/numbers/numbers.js
@@ -0,0 +1,23 @@
+// var e = 0.1, f = 0.2;
+
+// var result = e*f;
+// console.log(result);
+
+// var g = 1.23e16;
+// console.log(g);
+
+// var h = 012;
+// console.log(h);
+
+var j = parseInt("019",10);
+j = parseInt("1010111", 3);
+console.log(j);
+
+var k = parseInt("a 23 people",10)
+console.log(k);
+console.log(isNaN(k));
+
+var m = parseFloat("this: 123.456 is a strange number");
+console.log(k);
+
+var r = 15/10;
